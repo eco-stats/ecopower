a31e8eb181dc34b9440bb87d60577e4cbda04fb3
lsjmichelle <homosapien_ml@hotmail.com>
2020-12-26 06:15:43 +1100

    unlimited power!

---
+++ R/power_functions.R
@@ -0,0 +1,51 @@
+utils::globalVariables(c(
+  "object",
+  "N",
+  "coeffs",
+  "newdata",
+  "n_replicate",
+  "do.fit",
+  "test",
+  "term",
+  "object0"
+))
+
+MVApowerstat = function(stats, coeffs) {
+  stats.null = suppressMessages(
+    suppressWarnings(
+      anova(
+        suppressMessages(
+          suppressWarnings(
+            extend(
+              object=object,
+              N=N,
+              coeffs=coeffs,
+              newdata=newdata,
+              n_replicate=n_replicate,
+              do.fit=do.fit
+            )
+          )
+        ), 
+        nBoot=1, 
+        test=test,
+        show.time = "none"
+      )
+    )
+  )$table[term,3]
+}
+
+MVApowerstatObj = function(stats) {
+  object_sim = extend(
+    object=object,
+    N=N,
+    coeffs=coeffs,
+    newdata=newdata,
+    n_replicate=n_replicate,
+    do.fit=do.fit
+  )
+  extended_data = object_sim$data
+  object0_sim = update(object_sim, formula=object0$formula)
+  stats = anova(
+    object0_sim, object_sim, nBoot=1, test=test, show.time="none"
+  )$table[2,3]
+}
