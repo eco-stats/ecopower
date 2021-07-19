33a87231d656ce1fbf1ec0cbcd613a7733d6011b
lsjmichelle <homosapien_ml@hotmail.com>
2021-07-19 10:51:16 +1000

    update crayweed data

--- R/crayweed.R
+++ R/crayweed.R
@@ -9,22 +9,28 @@
 #'
 #' @format An object of class \code{"list"} containing:
 #' \describe{
-#'   \item{abund}{A matrix with 27 observations of abundance of 20 fish species.}
+#'   \item{abund}{A matrix with 27 observations of abundance of 34 fish species.}
 #'   \item{X}{A data frame with treatment and time variables.}
 #' }
 #' @details
 #' The matrix \code{abund} has the following species abundances:
 #' \itemize{
+#'   \item Abudefduf.sp.
+#'   \item Acanthopagrus.australis
 #'   \item Acanthurus.nigrofuscus
 #'   \item Achoerodus.viridis
 #'   \item Aplodactylus.lophodon
 #'   \item Atypichthys.strigatus
 #'   \item Cheilodactylus.fuscus
 #'   \item Chromis.hypsilepis
+#'   \item Crinodus.lophodon
 #'   \item Girella.elevata
 #'   \item Girella.tricuspidata
+#'   \item Hypoplectrodes.maccullochi
+#'   \item Needle.fish.unidentified
 #'   \item Notolabrus.gymnogenis
 #'   \item Odax.cyanomelas
+#'   \item Olisthops.cyanomelas
 #'   \item Ophthalmolepis.lineolatus
 #'   \item Parma.microlepis
 #'   \item Parma.unifasciata
@@ -32,9 +38,17 @@
 #'   \item Pempheris.multiradiata
 #'   \item Pictilabrus.laticlavius
 #'   \item Prionurus.microlepidotus
+#'   \item Pseudocaranx.dentex
+#'   \item Pseudojuloides.elongatus
 #'   \item Pseudolabrus.gymnogenis
+#'   \item Sardinops.neopilchardus
 #'   \item Scorpis.lineolatus
+#'   \item Seriola.lalandi
+#'   \item Sphyraena.obtusata
+#'   \item Tetractenos.hamiltoni
 #'   \item Trachinops.taeniatus
+#'   \item Trachurus.novaezelandiae
+#'   \item Upeneichthyes.lineatus
 #' }
 #' The data frame \code{X} has the following variables:
 #' \itemize{
